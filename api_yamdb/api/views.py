0dc4202ebd234003d599876c01aa15b338edd863
Владимир Шленсков <shlenskov@gmail.com>
2022-11-13 14:27:54 +0300

    My final wf commit 10

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -9,7 +9,6 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
 from reviews.models import Category, Genre, Review, Title
 from .filters import TitlesFilter
 from .permissions import (AdminOrReadOnly, AdminOrSuperUserOnly,
