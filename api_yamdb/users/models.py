c847ff2d0f6a1a2d8e45abf522d23f5d2a9227d9
Владимир Шленсков <shlenskov@gmail.com>
2022-11-13 15:06:58 +0300

    My final wf commit 12

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,6 +1,6 @@
+from core.validators import UserRegexValidator, validate_username
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from core.validators import validate_username, UserRegexValidator
 
 USER: str = 'user'
 MODERATOR: str = 'moderator'
